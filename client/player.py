09c57c75c7300b80eac3dc250780e28968085ae2
MOD0912 <melvin.kapferer@gmail.com>
2025-03-25 21:40:53 +0100

    Initialize gun in Player class and store initial gun position

--- client/player.py
+++ client/player.py
@@ -3,13 +3,19 @@ create a player class that will create the player
 '''
 import ursina as ur 
 from ursina.prefabs.first_person_controller import FirstPersonController
+from gun import Gun
 
 
 class Player(FirstPersonController):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, master, pos, *args, **kwargs):
+        super().__init__(position=pos[0], rotation=pos[1], *args, **kwargs)
+        # pos: [(position), (rotation), (gun_pos), (gun_rot)] of opponents
+        self.pos = pos
         #self.disable_movement()
-
+        
+        self.gun = Gun(master, self.pos[2], self.pos[3], ur.color.green)
+        self.gun_position = self.pos[2]  # Store initial gun position
+    
     def disable_movement(self):
         self.speed = 0
         self.jump_height = 0
@@ -19,7 +25,13 @@ class Player(FirstPersonController):
         if key == "alt":
             exit()
 
-   
+
+if __name__ == '__main__':
+    import main
+
+
+
+
 
 
 
