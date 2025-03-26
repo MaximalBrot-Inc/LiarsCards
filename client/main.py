20e78dd3037a6328eb3d398301d4e0b958f902cc
MOD0912 <melvin.kapferer@gmail.com>
2025-03-26 11:46:10 +0100

    Add player class with initial gun position and create test files for player data

--- client/main.py
+++ client/main.py
@@ -19,19 +19,20 @@ class Main:
         initialize the main class
         '''
         self.positions = {      # pos: [(position), (rotation), (gun_pos), (gun_rot)] of opponents
-            0: [(0, 0, -3), (0, 0, 0), (0, 1, -3), (0, 0, 0)],
-            1: [(3, 0, -1.5), (0, 275, 0), (3, 1, -1.5), (0, 60, 0)],
-            2: [(3, 0, 1.5), (0, 230, 0), (3, 1, 1.5), (0, 120, 0)],
-            3: [(0, 0, 3), (0, 180, 0), (0, 1, 3), (0, 180, 0)],
-            4: [(-3, 0, 1.5), (0, 100, 0), (-3, 1, 1.5), (0, 240, 0)],
-            5: [(-3, 0, -1.5), (0, 55, 0), (-3, 1, -1.5), (0, 300, 0)]
+            0: [(0, 0, -3), (0, 0, 0), (0, 1, -3), (0, 0, 0), ur.color.red],
+            1: [(0, 0, 3), (0, 180, 0), (0, 1, 3), (0, 180, 0), ur.color.blue],
+            2: [(-3, 0, -1.5), (0, 65, 0), (-3, 1, -1.5), (0, 300, 0), ur.color.green], 
+            3: [(3, 0, 1.5), (0, 245, 0), (3, 1, 1.5), (0, 120, 0), ur.color.yellow],
+            4: [(-3, 0, 1.5), (0, 115, 0), (-3, 1, 1.5), (0, 240, 0), ur.color.pink],
+            5: [(3, 0, -1.5), (0, 295, 0), (3, 1, -1.5), (0, 60, 0), ur.color.orange]
         }
+        self.index = 0
         
     def window(self):
         '''
         create the window
         '''
-        self.app = ur.Ursina(icon="../ASSets/rsz_leserunde.ico", window_title="3D Game", development_mode=debug)
+        self.app = ur.Ursina(icon="rsz_leserunde.ico", window_title="3D Game", development_mode=debug)
         #self.ui = UI()
         self.threed()
         
@@ -47,32 +48,37 @@ class Main:
         starts the 3D game
         '''
         #self.ui.start()
+        self.network = Network()
+        self.network.connect("127.0.0.1", 8000)
+        self.network.receive()
         sky = ur.Sky()
-        #model = ur.load_model('textures/untitled')
-
+        for i in self.positions:
+            print(self.positions[i][4])
         table = ur.Entity(
             model="untitled",
-            # model="cube",
             position=(0, 0, 0),
-            #scale=2,
-            # texture="assets/Tesla-Cybertruck-Metro-Look.jpg",
+            scale=1.5,
             shader=lit_with_shadows_shader
         )
         #tabletop = ur.Entity(model='circle', color="#5C4033", position=(0, 1.1, 0), rotation=(90, 0, 0), scale=(4, 4, 4))
-        player = Player(table, self.positions[5])
+        player = Player(table, self.positions[3])
         floor = ur.Entity(model='plane', scale=(100, 1, 100), color=ur.color.white.tint(-0.2), texture='white_cube', texture_scale=(100, 100), collider='box')
-        #enemy = Opponent(position=(3, 1, 0), scale=(1, 2, 1))
-        #enemy2 = Opponent(position=(0, 1, 3), scale=(1, 2, 1))
+
         
         #self.app.icon = "textures/Leserunde.ico"
+        ur.invoke(self.spawn_enemy, delay=2)
 
     def spawn_enemy(self):
         '''
         spawn the enemy
         '''
-        for i in range(6):
-            enemy = Opponent(position=self.positions[i][0], scale=(1, 2, 1))
-            enemy.rotation = self.positions[i][1]
+        i = self.index
+        if i == len(self.positions):
+            return
+        enemy = Opponent(position=self.positions[i][0], scale=(0.5, 0.5, 0.5), rotation = self.positions[i][1])
+        self.index += 1
+        #ur.invoke(self.spawn_enemy, delay=1)
+        self.spawn_enemy()
     
 
 main = Main()
